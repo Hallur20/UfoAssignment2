26023a531838a9c38b06ec518ab4c7f4503b04d7
hvn15 <Hallur20@github.com>
2019-11-10 00:25:37 +0100

    wqaiopekpqowi

---
+++ src/main/java/cphbusiness/ufo/letterfrequencies/Main.java
@@ -0,0 +1,77 @@
+package cphbusiness.ufo.letterfrequencies;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toMap;
+
+/**
+ * Frequency analysis Inspired by
+ * https://en.wikipedia.org/wiki/Frequency_analysis
+ *
+ * @author kasper
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+
+            String fileName = "/home/hvn15/letterfrequencies/src/main/resources/FoundationSeries.txt";
+            Reader reader = new FileReader(fileName);
+            Map<Integer, Long> freq = new HashMap<>();
+            tallyChars(reader, freq);
+            //print_tally(freq);
+
+
+    }
+
+    private static void tallyChars(Reader reader, Map<Integer, Long> freq) throws IOException {
+        int sum = 0;
+        for (int i = 0; i < 100; i++){
+            long start = System.currentTimeMillis();
+
+            int b;
+            while ((b = reader.read()) != -1) {
+                try {
+                    freq.put(b, freq.get(b) + 1);
+                } catch (NullPointerException np) {
+                    freq.put(b, 1L);
+                }
+            }
+            reader = new FileReader("/home/hvn15/letterfrequencies/src/main/resources/FoundationSeries.txt");
+            long finish = System.currentTimeMillis();
+            long timeElapsed = finish - start;
+            sum += timeElapsed;
+        }
+
+        System.out.println("average was: " + sum/100);
+
+
+
+    }
+
+    private static void print_tally(Map<Integer, Long> freq) {
+        int dist = 'a' - 'A';
+        Map<Character, Long> upperAndlower = new LinkedHashMap();
+        for (Character c = 'A'; c <= 'Z'; c++) {
+            upperAndlower.put(c, freq.getOrDefault(c, 0L) + freq.getOrDefault(c + dist, 0L));
+        }
+        Map<Character, Long> sorted = upperAndlower
+                .entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(
+                        toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                                LinkedHashMap::new));
+        for (Character c : sorted.keySet()) {
+            //System.out.println("" + c + ": " + sorted.get(c));;
+        }
+    }
+}
